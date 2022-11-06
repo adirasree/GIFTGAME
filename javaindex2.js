42d3470b9ee35d4183742b7606eb4f43057d4ec5
adirasree <111376459+adirasree@users.noreply.github.com>
2022-11-06 12:41:34 +0530

    Add files via upload

---
+++ javaindex2.js
@@ -0,0 +1,12 @@
+function giftgen() {
+    var n = document.getElementById("num").value;
+    for (let i = 1; i <= n; i++) {
+        const parent1 = document.getElementById("output");
+        const myImage = new Image(100, 200);
+        myImage.src = "box.jfif";
+        // myImage.setAttribute("class","gift");
+        parent1.appendChild(myImage);
+
+                
+    }
+}
